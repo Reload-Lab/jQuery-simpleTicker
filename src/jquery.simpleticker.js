77aaf4107a99399bf235f8429812d0b1c89fa07b
Reload - Laboratorio Multimediale <79582758+Reload-Lab@users.noreply.github.com>
2021-04-28 16:21:56 +0200

    Add files via upload

--- src/jquery.simpleticker.js
+++ src/jquery.simpleticker.js
@@ -1,168 +1,295 @@
 /***
  * Name: jQuery simpleticker plugin
- * Vesion: 1.0.0
+ * Vesion: 1.0.1
  * Author: Domenico Gigante
  * Repository: https://github.com/Reload-Lab/jQuery-simpleTicker
  ***/
 
 (function($){
 	
-	$.fn.extend({
+	$.fn.simpleticker = function(options){
 		
-		simpleticker: function(options){
+		var directions = [
+			'horizontal',
+			'vertical'
+		];
+		
+		var booleans = [
+			0,
+			1
+		];
+		
+		// Setup default settings
+		var defaults = $.extend(
+			{
+				velocity: 1,
+				forward: true,
+				direction: 'horizontal', // vertical
+				nodes: '.item',
+				wrapper: '.item-wrap',
+				pause: true,
+				complete: null
+			},
+			options
+		);
+		
+		this.each(function(){
 			
-			// Setup default settings
-			var settings = $.extend(
-				{
-					velocity: 1,
-					forward: true,
-					childNode: '.item',
-					childWrapper: '.item-wrap',
-					pauseOnHover: true,
-					complete: null
-				},
-				options
-			);
-
-			var curXPos;
-			var myReq;
-			var el = this;
-			var x0;
-			var x1;
-			var w0;
+			/*** data settings ***/
+			// merge default config with element setting
+			var settings = $.extend({}, defaults); 
+			
+			// data: velocity
+			if(typeof $(this).data('stVelocity') == 'number' && $(this).data('stVelocity') > 0){
+			
+				settings.velocity = $(this).data('stVelocity');
+			}
+			
+			// data: forward
+			if(typeof $(this).data('stForward') == 'boolean' || booleans.indexOf($(this).data('stForward')) != -1){
+			
+				settings.forward = !!$(this).data('stForward');
+			}
+			
+			// data: direction
+			if(typeof $(this).data('stDirection') == 'string' && directions.indexOf($(this).data('stDirection')) != -1){
+			
+				settings.direction = $(this).data('stDirection');
+			}
+			
+			// data: nodes
+			if(typeof $(this).data('stNodes') == 'string' && $($(this).data('stNodes')).length){
+			
+				settings.nodes = $(this).data('stNodes');
+			}
+			
+			// data: wrapper
+			if(typeof $(this).data('stWrapper') == 'string' && $($(this).data('stWrapper')).length){
+			
+				settings.wrapper = $(this).data('stWrapper');
+			}
+			
+			// data: pause
+			if(typeof $(this).data('stPause') == 'boolean' || booleans.indexOf($(this).data('stPause')) != -1){
+			
+				settings.pause = !!$(this).data('stPause');
+			}
+			
+			/*** set init variables ***/
+			var curPos; // current position
+			var reqAF; // requestAnimationFrame
+			var el = this; // this ticker
+			var t0; // initial translation
+			var t1; // final translation
+			var w; // ticker width/height
 			var v = settings.velocity; // velocity
-			var W = $(el).width();
+			var W = settings.direction == 'vertical'? $(el).height(): $(el).width(); // container width/height
+			var t = settings.direction == 'vertical'? 'translateY': 'translateX'; // translation
 			
+			/*** window onresize ***/
 			window.addEventListener('resize', function(){
 				
+				// update some variables
 				if(settings.forward){
 					
-					x0 = -1 * w0;
-					x1 = 0;
+					t0 = -w; // initial translation
+					t1 = 0; // final translation
 				} else{
 					
-					W = $(el).width();
+					W = settings.direction == 'vertical'? $(el).height(): $(el).width(); // container width/height
 					
-					x0 = -1 * (w0 - W);
-					x1 = -1 * (2 * w0 - W);
+					t0 = -(w - W); // initial translation
+					t1 = -(2 * w - W); // final translation
 				}
 			});
 			
+			/*** init setup ***/
+			// ticker container must be flex
+			$(el).css({
+				'display': 'flex'
+			});
+			
+			// if forward
 			if(settings.forward){
 				
-				w0 = $(el).find(settings.childWrapper).width();
-				
-				x0 = -1 * w0;
-				x1 = 0;
-				
-				curXPos = x0;
-
-				$(el).each(function(index, elem){
+				if(settings.direction == 'vertical'){
 					
-					$(elem).find(settings.childWrapper)
-						.each(function(index, elem){
-							
-							$($(elem).find($(settings.childNode).get().reverse()))
-								.each(function(){
-								
-									$(this).clone()
-										.prependTo(elem);
-								});
-						});
-				});
-			} else{
-				
-				w0 = $(el).find(settings.childWrapper).width();
-				
-				x0 = -1 * (w0 - W);
-				x1 = -1 * (2 * w0 - W);
-		  
-				if($(el).find(settings.childWrapper).css('transform') !== 'none'){
-					
-					curXPos = $(el).find(settings.childWrapper)
-						.css('transform')
-						.split(/[()]/)[1]
-						.split(',')[4];
+					// ticker container must be flex column
+					$(el).css({
+						'flex-direction': 'column',
+					});
+			
+					// ticker height
+					// wrapper must be flex column
+					w = $(el).find(settings.wrapper)
+						.css({
+							'display': 'flex',
+							'flex-direction': 'column',
+							'flex-wrap': 'nowrap',
+							'white-space': 'wrap'
+						})
+						.height();
 				} else{
 					
-					curXPos = 0;
+					// ticker width
+					// wrapper must be flex row
+					w = $(el).find(settings.wrapper)
+						.css({
+							'display': 'flex',
+							'flex-direction': 'row',
+							'flex-wrap': 'nowrap',
+							'white-space': 'nowrap'
+						})
+						.width();
 				}
-
-				$(el).each(function(index, elem){
-					
-					$(elem).find(settings.childWrapper)
-						.each(function(index, elem){
+				
+				t0 = -w; // initial translation
+				t1 = 0; // final translation
+				
+				// set actual position
+				curPos = t0;
+				
+				// clone child nodes and prepend to child wrapper
+				$(el).find(settings.wrapper)
+					.each(function(index, elem){
+						
+						$($(elem).find($(settings.nodes).get().reverse()))
+							.each(function(){
 							
-							$(elem).find(settings.childNode)
-								.each(function(){
-									
-									$(this).clone()
-										.appendTo(elem);
-								});
-						});
-				});
+								$(this).clone()
+									.prependTo(elem);
+							});
+					});
+			} else{
+				
+				if(settings.direction == 'vertical'){
+					
+					// ticker container must be flex column
+					$(el).css({
+						'flex-direction': 'column',
+					});
+			
+					// ticker height
+					// wrapper must be flex column
+					w = $(el).find(settings.wrapper)
+						.css({
+							'display': 'flex',
+							'flex-direction': 'column',
+							'flex-wrap': 'nowrap',
+							'white-space': 'wrap'
+						})
+						.height();
+				} else{
+					
+					// ticker width
+					// wrapper must be flex row
+					w = $(el).find(settings.wrapper)
+						.css({
+							'display': 'flex',
+							'flex-direction': 'row',
+							'flex-wrap': 'nowrap',
+							'white-space': 'nowrap'
+						})
+						.width();
+				}
+				
+				t0 = -(w - W); // initial translation
+				t1 = -(2 * w - W); // final translation
+		  	
+				// set actual position
+				curPos = t0
+				
+				// clone child nodes and append to child wrapper
+				$(el).find(settings.wrapper)
+					.each(function(index, elem){
+						
+						$(elem).find(settings.nodes)
+							.each(function(){
+								
+								$(this).clone()
+									.appendTo(elem);
+							});
+					});
 			}
 			
+			/*** move ticker by step ***/
 			function step(){
 				
 				if(settings.forward){
 					
-					if(curXPos <= 0){
+					// if actual position is minus or equal to zero,
+					// go ahead
+					if(curPos <= t1){
 						
-						curXPos = curXPos + 1 * v;
+						curPos = curPos + 1 * v;
 						
-						$(el).find(settings.childWrapper)
-							.css('transform', 'translateX(' + curXPos + 'px)');
-					} else{
+						$(el).find(settings.wrapper)
+							.css('transform', t + '(' + curPos + 'px)');
+					}
+					// else set actual position to start position
+					else{
 						
-						$(el).find(settings.childWrapper)
-							.css('transform', 'translateX(' + x0 + ')');
+						$(el).find(settings.wrapper)
+							.css('transform', t + '(' + t0 + ')');
 						
-						curXPos = x0;
+						curPos = t0;
+					
+						// call complete function
+						if($.isFunction(settings.complete)){
+							
+							settings.complete.call(this, el);
+						}
 					}
 				} else{
 					
-					if(curXPos >= x1){
+					// if actual position is major or equal to end position,
+					// go ahead
+					if(curPos >= t1){
 						
-						curXPos = curXPos - 1 * v;
+						curPos = curPos - 1 * v;
 						
-						$(el).find(settings.childWrapper)
-							.css('transform', 'translateX(' + curXPos + 'px)');
-					} else{
+						$(el).find(settings.wrapper)
+							.css('transform', t + '(' + curPos + 'px)');
+					}
+					// else set actual position to start position
+					else{
 						
-						$(el).find(settings.childWrapper)
-							.css('transform', 'translateX(' + x0 + ')');
+						$(el).find(settings.wrapper)
+							.css('transform', t + '(' + t0 + ')');
 						
-						curXPos = x0;
+						curPos = t0;
+						
+						// call complete function
+						if($.isFunction(settings.complete)){
+							
+							settings.complete.call(this, el);
+						}
 					}
 				}
-
-				myReq = window.requestAnimationFrame(step);
+	
+				reqAF = window.requestAnimationFrame(step);
 			}
-
-			myReq = window.requestAnimationFrame(step);
-
-			if(settings.pauseOnHover){
+	
+			reqAF = window.requestAnimationFrame(step);
+			
+			/*** manage pause on hover ***/
+			if(settings.pause){
 				
-				$(this).hover(
+				$(el).hover(
 					function(){
 						
-						cancelAnimationFrame(myReq);
+						// on mouse over
+						// stop animation
+						window.cancelAnimationFrame(reqAF);
 					},
 					function(){
 						
-						myReq = window.requestAnimationFrame(step);
+						// on mouse out
+						// restart animation
+						reqAF = window.requestAnimationFrame(step);
 					}
 				);
 			}
-
-			return this.each(function(){
-				
-				if($.isFunction(settings.complete)){
-					
-					settings.complete.call(this);
-				}
-			});
-		}
-	});
+		});
+	}
 })(jQuery);
